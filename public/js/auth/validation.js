34e1ff8b05bbbb86c2e500a9d717c16799744ddf
Mario González <mario@mariogl.com>
2024-03-02 21:00:02 +0100

    Initial commit

---
+++ public/js/auth/validation.js
@@ -0,0 +1,55 @@
+export const isValidUsername = (username) => {
+  let isValid;
+
+  // Incluye aquí tu implementación para validar el nombre de usuario
+  // Tienes a tu disposición una variable llamada username
+  // que contiene el valor del campo de nombre de usuario
+  //
+  // La variable isValid debe apuntar a true o a false
+  //
+  // Recuerda borrar estos comentarios
+
+  return isValid;
+};
+
+export const isValidPassword = (password) => {
+  let isValid;
+
+  // Incluye aquí tu implementación para validar el nombre de usuario
+  // Tienes a tu disposición una variable llamada password
+  // que contiene el valor del campo de contraseña
+  //
+  // La variable isValid debe apuntar a true o a false
+  //
+  // Recuerda borrar estos comentarios
+
+  return isValid;
+};
+
+export const isSamePassword = (password1, password2) => {
+  let isSame;
+
+  // Incluye aquí tu implementación para validar el nombre de usuario
+  // Tienes a tu disposición una variable llamada password1 y otra password2
+  // que contienen los valores de los campos de contraseñas
+  //
+  // La variable isSame debe apuntar a true o a false
+  //
+  // Recuerda borrar estos comentarios
+
+  return isSame;
+};
+
+export const isAdultAge = (age) => {
+  let isAdult;
+
+  // Incluye aquí tu implementación para validar la edad
+  // Tienes a tu disposición una variable llamada age
+  // que contiene el valor del campo de edad
+  //
+  // La variable isAdult debe apuntar a true o a false
+  //
+  // Recuerda borrar estos comentarios
+
+  return isAdult;
+};
