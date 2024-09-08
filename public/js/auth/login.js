f2823c21cb654f94dcecd379adcf5f7976f5d719
Aniol Rodriguez <arodrigue96@gmail.com>
2024-09-08 19:41:51 +0200

    primer commit

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -2,15 +2,8 @@ export const checkCredentials = (username, password) => {
   const storedUsername = localStorage.getItem("username");
   const storedPassword = localStorage.getItem("password");
 
-  let areValidCredentials;
-
-  // Incluye aquí tu implementación para validar las credenciales
-  // Tienes a tu disposición una variable llamada username y otra llamada password
-  // que contienen el nombre de usuario y la contraseña que el usuario ha introducido
-  //
-  // La variable areValidCredentials debe apuntar a true o a false
-  //
-  // Recuerda borrar estos comentarios
+  const areValidCredentials =
+    storedUsername === username && storedPassword === password;
 
   return areValidCredentials;
 };
--- public/js/auth/validation.js
+++ public/js/auth/validation.js
@@ -1,55 +1,30 @@
 export const isValidUsername = (username) => {
-  let isValid;
-
-  // Incluye aquí tu implementación para validar el nombre de usuario
-  // Tienes a tu disposición una variable llamada username
-  // que contiene el valor del campo de nombre de usuario
-  //
-  // La variable isValid debe apuntar a true o a false
-  //
-  // Recuerda borrar estos comentarios
+  const isValid = username.length >= 5;
 
   return isValid;
 };
 
 export const isValidPassword = (password) => {
-  let isValid;
+  // Expresiones regulares utilizadas:
+  const containUpperCase = /[A-Z]/;
+  const containNumber = /[0-9]/;
 
-  // Incluye aquí tu implementación para validar la password
-  // Tienes a tu disposición una variable llamada password
-  // que contiene el valor del campo de contraseña
-  //
-  // La variable isValid debe apuntar a true o a false
-  //
-  // Recuerda borrar estos comentarios
+  const isValid =
+    password.length >= 8 &&
+    containUpperCase.test(password) &&
+    containNumber.test(password);
 
   return isValid;
 };
 
 export const isSamePassword = (password1, password2) => {
-  let isSame;
-
-  // Incluye aquí tu implementación para validar que las dos passwords son iguales
-  // Tienes a tu disposición una variable llamada password1 y otra password2
-  // que contienen los valores de los campos de contraseñas
-  //
-  // La variable isSame debe apuntar a true o a false
-  //
-  // Recuerda borrar estos comentarios
+  const isSame = password1 === password2;
 
   return isSame;
 };
 
 export const isAdultAge = (age) => {
-  let isAdult;
-
-  // Incluye aquí tu implementación para validar la edad
-  // Tienes a tu disposición una variable llamada age
-  // que contiene el valor del campo de edad
-  //
-  // La variable isAdult debe apuntar a true o a false
-  //
-  // Recuerda borrar estos comentarios
+  const isAdult = age >= 18;
 
   return isAdult;
 };
